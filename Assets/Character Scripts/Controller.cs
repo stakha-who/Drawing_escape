fffc932be1a81400849b6d74b047a52fed54097c
sererree <catcat29626@gmail.com>
2024-05-05 20:26:09 +0300

    Character Controller
    он чут чут бесконечно прыгает

---
+++ Assets/Character Scripts/Controller.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Rigidbody2D Rb;
+    public float speed = 10f;
+    public float jump = 1f;
+    public float JumpTimer = 1;
+
+    public GameObject Character;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (JumpTimer < 1)
+        {
+            JumpTimer += Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Rb.AddForce(Vector2.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Rb.AddForce(Vector2.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (JumpTimer >= 1)
+            {
+                Rb.AddForce(Vector2.up * jump);
+                JumpTimer = 0; 
+            }
+            
+
+        }
+
+
+    }
+}
