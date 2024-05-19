b47ff9eec54e7b0e931a7b81d6177e28533212e1
sererree <catcat29626@gmail.com>
2024-05-19 19:48:20 +0300

    Main Menu Picture

--- Assets/Scripts/TriggerPictureEgypt.cs
+++ Assets/Scripts/TriggerPictureEgypt.cs
@@ -6,9 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class TriggerPicture : MonoBehaviour
 {
-    //public GameObject Picture;
-    //public GameObject Character;
-
     public void OnTriggerEnter2D(Collider2D collision)
     {
         SceneManager.LoadScene(2);
